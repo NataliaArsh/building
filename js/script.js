4f5577593ddbe9353ec4476fbdb30f7b5c77cbe2
NataliaArsh <nata-arsh@mail.ru>
2021-11-08 15:22:47 +0300

    bem

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
     const header = document.querySelector('.header');
     window.onscroll = () => {
         if (window.pageYOffset > 50) {
-            header.classList.add('header_active');
+            header.classList.add('header__active');
         } else {
-            header.classList.remove('header_active');
+            header.classList.remove('header__active');
         }
     }
 }());
@@ -14,19 +14,19 @@
 
 (function () {
     const burgerItem = document.querySelector('.burger');
-    const menu = document.querySelector('.header_nav');
+    const menu = document.querySelector('.header__nav');
     const menuCloseItem = document.querySelector('.header__nav-close');
-    const menuLinks = document.querySelectorAll('.header_link');
+    const menuLinks = document.querySelectorAll('.header__link');
     burgerItem.addEventListener('click', () => {
-        menu.classList.add('header_nav_active'); 
+        menu.classList.add('header__nav-active'); 
     });
     menuCloseItem.addEventListener('click', () => {
-        menu.classList.remove('header_nav_active');
+        menu.classList.remove('header__nav-active');
     });
     if (window.innerWidth < 768) {
         for (let i=0; i< menuLinks.length; i += 1) {
             menuLinks[i].addEventListener('click', () => {
-                menu.classList.remove('header_nav_active');
+                menu.classList.remove('header__nav-active');
             });
         }
     }
